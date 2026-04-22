66e437d4a6a7ebe7e8cd769c99701d7f7515974b
RefinedStone <chem.en.9273@gmail.com>
2026-04-22 09:23:57 +0900

    refactor(shell): add delegation surface helper

---
+++ src/adapter/inbound/tui/app/shell_presentation/overlays/popup/planning_init_copy/review/simple_review/view/entry/contract_handoff/wiring/surface_handoff/delegation/surface/delegation.rs
@@ -0,0 +1,9 @@
+use super::super::super::super::super::super::super::super::super::super::super::PlanningInitOverlayView;
+use super::super::super::super::super::super::super::super::super::super::super::copy::PlanningSimpleReviewCopy;
+use super::super::super::super::surface;
+
+pub(super) fn build_simple_review_overlay_view_from_copy(
+    copy: PlanningSimpleReviewCopy,
+) -> PlanningInitOverlayView {
+    surface::build_simple_review_overlay_view_from_copy(copy)
+}
